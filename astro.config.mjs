3b4425be83348dc004e60b367d521ebd02ec3757
manumkd34 <manu.mukendi95@gmail.com>
2025-05-22 21:34:48 +0200

    Update configuration and dependencies for local development and deployment
    - Commented out the base URL in `astro.config.mjs` for local development.
    - Updated `package.json` to specify the package manager as `pnpm@8.6.0` and added `vite` as a dependency.
    - Modified `pnpm-lock.yaml` to reflect updated dependencies and versions.
    - Enhanced GitHub Actions workflow in `deploy.yml` to set up Node.js, install `pnpm`, and build the project before uploading artifacts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://manumkd34.github.io',
-  base:'PM',
+  // base:'PM', // Commenté pour le développement local
   trailingSlash: 'always',
   integrations: [tailwind()],
 });
